28e946873465a7d9d65881819e884d26f1d8f664
Flict-dev <zhdanom.maksi@gmail.com>
2021-12-22 19:49:08 +0300

    added tests

--- backend/tests.py
+++ backend/tests.py
@@ -1,7 +1,11 @@
 from fastapi.testclient import TestClient
-from main import app
+from main import app, SECRET_KEY
+from utils.crypt import Decoder
 
 client = TestClient(app)
+decoder = Decoder(SECRET_KEY)
+
+test_session = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJuYW1lIjoiJDJiJDEyJHNvMy9BcUc0STZpLnc3aGdqdU5KanVyeldlMG1zMXFiNUFCZmFMdHZsOWgxeS92UHVWUW9DIiwicGFzc3dvcmQiOiJUZXN0MTIzNDU2In0.cG6wkGC3Ff6-uDwQw5RfHtIOmu-_dLiUEFQCThB7bQI'
 
 
 def test_main():
@@ -10,12 +14,13 @@ def test_main():
 
 
 def test_creation_room():
-    response = client.post('/', json={'name': 'test', 'password': 'Test12345'})
+    response = client.post(
+        '/', json={'name': 'test3', 'password': 'Test123456'}
+    )
     assert response.status_code == 302
     assert list(response.json().keys())[0] == 'link'
     assert response.headers['Location'] == response.json()['link'] + '/auth'
     assert response.headers['Content-Type'] == 'application/json'
-    assert response.cookies != None
 
 
 def test_fail_creation_room():
@@ -26,3 +31,45 @@ def test_fail_creation_room():
         "error": 'password'
     }
     assert response.headers['Content-Type'] == 'application/json'
+
+
+def test_room_auth():
+    response = client.get(
+        '/rooms/so3slashAqG4I6i.w7hgjuNJjurzWe0ms1qb5ABfaLtvl9h1yslashvPuVQoC/auth',
+        cookies={'session': test_session}
+    )
+    assert response.status_code == 200
+    assert response.headers['Content-Type'] == 'text/html; charset=utf-8'
+
+
+def test_fail_with_incorrect_session_room_auth():
+    response = client.get(
+        '/rooms/so3slashAqG4I6i.w7hgjuNJjurzWe0ms1qb5ABfaLtvl9h1yslashvPuVQoC/auth',
+        cookies={'session': test_session+'error'}
+    )
+    assert response.status_code == 400
+
+
+def test_fail_without_cookies_room_auth():
+    response = client.get(
+        '/rooms/so3slashAqG4I6i.w7hgjuNJjurzWe0ms1qb5ABfaLtvl9h1yslashvPuVQoC/auth'
+    )
+    assert response.status_code == 401
+
+
+def test_password_room_auth():
+    response = client.post(
+        '/rooms/so3slashAqG4I6i.w7hgjuNJjurzWe0ms1qb5ABfaLtvl9h1yslashvPuVQoC/auth',
+        json={"password": "Test123456"}
+    )
+    assert response.status_code == 302
+    assert response.cookies.get('session') == test_session
+
+
+def test_invalid_password_room_auth():
+    response = client.post(
+        '/rooms/so3slashAqG4I6i.w7hgjuNJjurzWe0ms1qb5ABfaLtvl9h1yslashvPuVQoC/auth',
+        json={"password": "Tests123456"}
+    )
+    assert response.status_code == 401
+    assert response.json() == {'Erorr': 'Invalid password'}
