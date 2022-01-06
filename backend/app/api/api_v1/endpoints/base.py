a59f5fb35eb76c8bfac8e17c230229180a5a85ae
Flict-dev <zhdanom.maksi@gmail.com>
2022-01-07 02:14:40 +0300

    Delete templates from backend

--- backend/app/api/api_v1/endpoints/base.py
+++ backend/app/api/api_v1/endpoints/base.py
@@ -1,63 +1,42 @@
-from fastapi import (
-    APIRouter,
-    HTTPException,
-    status,
-    Request
-)
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import HTMLResponse
+from fastapi import APIRouter, status, Request
 from fastapi.responses import JSONResponse
-from utils.helpers import Parser, Reader
 from shemas.room import RoomReq
 from core.utils import parser, encoder, database
 
 router = APIRouter()
 
-# delete this
-templates = {
-    'index': Reader('index.html').read_html(),
-    'room': Reader('room.html').read_html(),
-    'room_auth': Reader('room_auth.html').read_html(),
-}
 
-@router.get('/')
+@router.get("/")
 async def home(request: Request):
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={'ip': f'{request.client.host}'},
+        content={"ip": f"{request.client.host}"},
     )
 
 
-@router.post('/')
+@router.post("/")
 async def create_room(room: RoomReq):
-        plainName, plainPassword = parser.parse_room_data(room)
-        hashed_name = encoder.hash_name(plainName)
-        hashed_password = encoder.hash_password(plainPassword)
-        link = encoder.gen_hash_link(hashed_name)
-        if hashed_password:
-            database.create_room(
-                {
-                    'name': hashed_name,
-                    'password': hashed_password
-                }
-            )
-            sessionCookie = encoder.encode_session(
-                hashed_name, plainPassword, 'Admin', encoder.key
-            )
-            return JSONResponse(
-                status_code=status.HTTP_201_CREATED,
-                content={'link': f'/rooms/{link}'},
-                headers={
-                    'Content-Type': 'application/json',
-                    'Set-Cookie': f'session={sessionCookie}',
-                }
-            )
+    plainName, plainPassword = parser.parse_room_data(room)
+    hashed_name = encoder.hash_name(plainName)
+    hashed_password = encoder.hash_password(plainPassword)
+    link = encoder.gen_hash_link(hashed_name)
+    if hashed_password:
+        database.create_room({"name": hashed_name, "password": hashed_password})
+        sessionCookie = encoder.encode_session(
+            hashed_name, plainPassword, "Admin", encoder.key
+        )
         return JSONResponse(
-                status_code=status.HTTP_400_BAD_REQUEST,
-                content={
-                    'error': 'Eliot hates when the password is too simple'
-                },
-                headers={
-                    'Content-Type': 'application/json',
-                }
-            )
+            status_code=status.HTTP_201_CREATED,
+            content={"link": f"/rooms/{link}"},
+            headers={
+                "Content-Type": "application/json",
+                "Set-Cookie": f"session={sessionCookie}",
+            },
+        )
+    return JSONResponse(
+        status_code=status.HTTP_400_BAD_REQUEST,
+        content={"error": "Eliot hates when the password is too simple"},
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
--- backend/app/utils/crypt.py
+++ backend/app/utils/crypt.py
@@ -7,7 +7,7 @@ from cryptography.fernet import Fernet
 class Validate:
     def __init__(self) -> None:
         self.pattern_password: re = re.compile(
-            r'^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z$%!#^]{8,}$'
+            r"^(?=.*[0-9].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z$%!#^]{8,}$"
         )
 
     def password(self, password) -> bool:
@@ -15,8 +15,8 @@ class Validate:
 
 
 class Encoder:
-    def __init__(self, sessionKey: str, sessionAlg: str = 'HS256') -> None:
-        self.context = CryptContext(schemes=['bcrypt'])
+    def __init__(self, sessionKey: str, sessionAlg: str = "HS256") -> None:
+        self.context = CryptContext(schemes=["bcrypt"])
         self.validate = Validate()
         self.sessionKey = sessionKey
         self.sessionAlg = sessionAlg
@@ -25,12 +25,10 @@ class Encoder:
     def key(self):
         key = Fernet.generate_key()
         print(key)
-        return str(key, encoding='utf-8')
+        return str(key, encoding="utf-8")
 
     def gen_hash_link(self, name: str) -> str:
-        return name.split('$')[-1]\
-            .replace("/", 'slash')\
-            .replace("\\", 'hsals')
+        return name.split("$")[-1].replace("/", "slash").replace("\\", "hsals")
 
     def hash_password(self, password: str) -> str:
         if self.validate.password(password):
@@ -42,16 +40,18 @@ class Encoder:
             return self.context.hash(name)
         raise ValueError("name")
 
-    def encode_session(self, roomName: str, roomPassword: str, username: str, msg_key: str) -> str:
+    def encode_session(
+        self, roomName: str, roomPassword: str, username: str, msg_key: str
+    ) -> str:
         return jwt.encode(
             payload={
                 "name": roomName,
                 "password": roomPassword,
                 "username": username,
-                "msg_key": msg_key
+                "msg_key": msg_key,
             },
             key=self.sessionKey,
-            algorithm=self.sessionAlg
+            algorithm=self.sessionAlg,
         )
 
     def encrypt_message(self, message: str, msg_key: str) -> str:
@@ -61,14 +61,16 @@ class Encoder:
 
 
 class Decoder:
-    def __init__(self, sessionKey: str, sessionAlg: str = 'HS256') -> None:
-        self.context = CryptContext(schemes=['bcrypt'])
+    def __init__(self, sessionKey: str, sessionAlg: str = "HS256") -> None:
+        self.context = CryptContext(schemes=["bcrypt"])
         self.sessionKey = sessionKey
         self.sessionAlg = sessionAlg
         self.encoder = Encoder(sessionKey)
 
     def decode_session(self, session: str) -> dict:
-        return dict(jwt.decode(session, key=self.sessionKey, algorithms=self.sessionAlg))
+        return dict(
+            jwt.decode(session, key=self.sessionKey, algorithms=self.sessionAlg)
+        )
 
     def verify_password(self, password: str, hashed_password: str) -> bool:
         return self.context.verify(password, hashed_password)
@@ -76,28 +78,26 @@ class Decoder:
     def verify_name(self, name: str, hashed_name: str) -> bool:
         return self.context.verify(name, hashed_name)
 
-    def verify_session(self, name: str, password: str, session: str, admin: bool = False) -> bool:
+    def verify_session(
+        self, name: str, password: str, session: str, admin: bool = False
+    ) -> bool:
         decoded_session = self.decode_session(session)
-        room_name, room_password = decoded_session['name'], decoded_session['password']
+        room_name, room_password = decoded_session["name"], decoded_session["password"]
         if admin:
             return (
-                room_name == name and self.verify_password(
-                    room_password, password
-                ) and decoded_session['username'] == 'Admin'
+                room_name == name
+                and self.verify_password(room_password, password)
+                and decoded_session["username"] == "Admin"
             )
-        return (
-            room_name == name and self.verify_password(
-                room_password, password
-            )
-        )
+        return room_name == name and self.verify_password(room_password, password)
 
     def session_add_key(self, session: str, msg_key: str) -> str:
         decoded_session = self.decode_session(session)
         return self.encoder.encode_session(
-            decoded_session['name'], 
-            decoded_session['password'],
-            decoded_session['username'],
-            msg_key
+            decoded_session["name"],
+            decoded_session["password"],
+            decoded_session["username"],
+            msg_key,
         )
 
     def dcrypt_message(self, encoded_msg: str, msg_key: str) -> str:
