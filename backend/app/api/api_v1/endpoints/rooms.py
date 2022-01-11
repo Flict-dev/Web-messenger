710d801945652e9314ef1d4a8cf33a6cd6d1551b
Flict-dev <zhdanom.maksi@gmail.com>
2022-01-12 00:47:16 +0300

    Exceptions refactor

--- backend/app/api/api_v1/endpoints/rooms.py
+++ backend/app/api/api_v1/endpoints/rooms.py
@@ -15,261 +15,211 @@ from shemas.room import RoomAuth
 from shemas.user import UserBlock
 from shemas.msg_key import MsgKeysCreate
 from core.utils import manager, parser, encoder, decoder, database
+
 router = APIRouter()
 
 
 @router.get("/{name}/auth")
 async def room_session_auth(name: str, session: Optional[str] = Cookie(None)):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            user = database.get_user_by_name(
-                decoder.decode_session(session)["username"], room.id
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        user = database.get_user_by_name(
+            decoder.decode_session(session)["username"], room.id
+        )
+        if decoder.verify_session(hashed_name, room.password, session) and user.status:
+            return JSONResponse(
+                status_code=status.HTTP_302_FOUND,
+                content={"Auth": True},
+                headers={
+                    "Content-Type": "application/json",
+                    "Location": f"api/v1/rooms/{name}",
+                },
             )
-            if (
-                decoder.verify_session(hashed_name, room.password, session)
-                and user.status
-            ):
-                return JSONResponse(
-                    status_code=status.HTTP_302_FOUND,
-                    content={"Auth": True},
-                    headers={
-                        "Content-Type": "application/json",
-                        "Location": f"api/v1/rooms/{name}",
-                    },
-                )
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={
-                "Content-Type": "application/json",
-            },
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
 
 
 @router.post("/{name}/auth")
 async def room_password_auth(name: str, auth: RoomAuth):
-    try:
-        hashed_name = parser.parse_link_hash(name)
-        json_data = jsonable_encoder(auth)
-        room = database.get_room_by_name(hashed_name)
-        password, username = json_data["password"], json_data["username"]
-        if decoder.verify_password(password, room.password):
-            if not database.check_user(username, room.id):
-                database.create_user(
-                    {
-                        "name": username,
-                        "admin": False,
-                        "room_id": room.id,
-                    },
-                    room.id,
-                )
-                sessionCookie = encoder.encode_session(
-                    hashed_name, password, username, ""
-                )
-                return JSONResponse(
-                    status_code=status.HTTP_302_FOUND,
-                    content={"User": username},
-                    headers={
-                        "Content-Type": "application/json",
-                        "Set-Cookie": f"session={sessionCookie}",
-                        "Location": f"/rooms/{name}",
-                    },
-                )
-        return JSONResponse(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            content={"Erorr": "Invalid password or username"},
-            headers={
-                "Content-Type": "application/json",
-            },
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+    hashed_name = parser.parse_link_hash(name)
+    json_data = jsonable_encoder(auth)
+    room = database.get_room_by_name(hashed_name)
+    password, username = json_data["password"], json_data["username"]
+    if decoder.verify_password(password, room.password):
+        if not database.check_user(username, room.id):
+            database.create_user(
+                {
+                    "name": username,
+                    "admin": False,
+                    "room_id": room.id,
+                },
+                room.id,
+            )
+            sessionCookie = encoder.encode_session(hashed_name, password, username, "")
+            return JSONResponse(
+                status_code=status.HTTP_302_FOUND,
+                content={"User": username},
+                headers={
+                    "Content-Type": "application/json",
+                    "Set-Cookie": f"session={sessionCookie}",
+                    "Location": f"/rooms/{name}",
+                },
+            )
+    return JSONResponse(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        content={"Erorr": "Invalid password or username"},
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
 
 
 @router.get("/{name}")
 async def room(name: str, session: Optional[str] = Cookie(None)):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            decoded_session = decoder.decode_session(session)
-            user = database.get_user_by_name(decoded_session["username"], room.id)
-            if (
-                decoder.verify_session(hashed_name, room.password, session)
-                and user.status
-            ):
-                encoded_messages, messages = database.get_all_messages(room.id), {}
-                for message in encoded_messages:
-                    user = database.get_user_by_id(message.user_id)
-                    messages[user.name] = {
-                        "Message": message.data,
-                        "Created_at": parser.parse_msg_time(message.created_at),
-                        "Status": user.status,
-                    }
-                return JSONResponse(
-                    status_code=status.HTTP_200_OK,
-                    content={
-                        "Status": 200,
-                        "User": decoded_session["username"],
-                        "Messages": messages,
-                        "Users": list(map(lambda user: user.name, room.users)),
-                    },
-                    headers={
-                        "Content-Type": "application/json",
-                        "Connection": "keep-alive",
-                    },
-                )
-        return JSONResponse(
-            status_code=status.HTTP_302_FOUND,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        decoded_session = decoder.decode_session(session)
+        user = database.get_user_by_name(decoded_session["username"], room.id)
+        if decoder.verify_session(hashed_name, room.password, session) and user.status:
+            encoded_messages, messages = database.get_all_messages(room.id), {}
+            for message in encoded_messages:
+                user = database.get_user_by_id(message.user_id)
+                messages[user.name] = {
+                    "Message": message.data,
+                    "Created_at": parser.parse_msg_time(message.created_at),
+                    "Status": user.status,
+                }
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={
+                    "Status": 200,
+                    "User": decoded_session["username"],
+                    "Messages": messages,
+                    "Users": list(map(lambda user: user.name, room.users)),
+                },
+                headers={
+                    "Content-Type": "application/json",
+                    "Connection": "keep-alive",
+                },
+            )
+    return JSONResponse(
+        status_code=status.HTTP_302_FOUND,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
+    )
 
 
 @router.delete("/{name}")
 async def delete_room(name: str, session: Optional[str] = Cookie(None)):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            user = database.get_user_by_name(
-                decoder.decode_session(session)["username"], room.id
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        user = database.get_user_by_name(
+            decoder.decode_session(session)["username"], room.id
+        )
+        if (
+            decoder.verify_session(hashed_name, room.password, session, True)
+            and user.status
+        ):
+            database.delete_room(room.id)
+            return JSONResponse(
+                status_code=status.HTTP_302_FOUND,
+                content={"Status": "Room has been deleted"},
+                headers={
+                    "Location": "/",
+                },
             )
-            if (
-                decoder.verify_session(hashed_name, room.password, session, True)
-                and user.status
-            ):
-                database.delete_room(room.id)
-                return JSONResponse(
-                    status_code=status.HTTP_302_FOUND,
-                    content={"Status": "Room has been deleted"},
-                    headers={
-                        "Location": "/",
-                    },
-                )
-        return JSONResponse(
-            status_code=status.HTTP_302_FOUND,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+    return JSONResponse(
+        status_code=status.HTTP_302_FOUND,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
+    )
 
 
 @router.patch("/{name}")
 async def block_room_user(
     name: str, user: UserBlock, session: Optional[str] = Cookie(None)
 ):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            username = jsonable_encoder(user)["username"]
-            user = database.get_user_by_name(username, room.id)
-            if (
-                decoder.verify_session(hashed_name, room.password, session, True)
-                and user.status
-            ):
-                await database.block_user(username, room.id)
-                return JSONResponse(
-                    status_code=status.HTTP_200_OK,
-                    content={"Status": "User has been blocked"},
-                )
-        return JSONResponse(
-            status_code=status.HTTP_302_FOUND,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        username = jsonable_encoder(user)["username"]
+        user = database.get_user_by_name(username, room.id)
+        if (
+            decoder.verify_session(hashed_name, room.password, session, True)
+            and user.status
+        ):
+            await database.block_user(username, room.id)
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"Status": "User has been blocked"},
+            )
+    return JSONResponse(
+        status_code=status.HTTP_302_FOUND,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
+    )
 
 
 @router.post("/{name}/key")
 async def create_msg_key(
     name: str, keyData: MsgKeysCreate, session: Optional[str] = Cookie(None)
 ):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            encoded_data = jsonable_encoder(keyData)
-            user = database.get_user_by_name(
-                decoder.decode_session(session)["username"], room.id
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        encoded_data = jsonable_encoder(keyData)
+        user = database.get_user_by_name(
+            decoder.decode_session(session)["username"], room.id
+        )
+        if (
+            decoder.verify_session(hashed_name, room.password, session, True)
+            and user.status
+        ):
+            database.create_msg_key(
+                room.id, encoded_data["destinied_for"], encoded_data["key"]
             )
-            if (
-                decoder.verify_session(hashed_name, room.password, session, True)
-                and user.status
-            ):
-                database.create_msg_key(
-                    room.id, encoded_data["destinied_for"], encoded_data["key"]
-                )
-                return JSONResponse(
-                    content={"Status": "Msg created"}, status_code=status.HTTP_200_OK
-                )
-        return JSONResponse(
-            status_code=status.HTTP_302_FOUND,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+            return JSONResponse(
+                content={"Status": "Msg created"}, status_code=status.HTTP_200_OK
+            )
+    return JSONResponse(
+        status_code=status.HTTP_302_FOUND,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
+    )
 
 
 @router.delete("/{name}/key")
 async def delete_msg_key(name: str, session: Optional[str] = Cookie(None)):
-    try:
-        if session:
-            hashed_name = parser.parse_link_hash(name)
-            room = database.get_room_by_name(hashed_name)
-            decoded_session = decoder.decode_session(session)
-            user = database.get_user_by_name(decoded_session["username"], room.id)
-            if (
-                decoder.verify_session(hashed_name, room.password, session)
-                and user.status
-            ):
-                try:
-                    data = database.get_msg_key(room.id, decoded_session["username"])
-                    key_session = decoder.session_add_key(session, data.key)
-                    await database.delete_key(data.id)
-                    return JSONResponse(
-                        status_code=status.HTTP_200_OK,
-                        content={"Status": "The encryption key was received"},
-                        headers={
-                            "Set-Cookie": f"session={key_session}",
-                        },
-                    )
-                except ValueError as error:
-                    return JSONResponse(
-                        content={"error": error},
-                        status_code=status.HTTP_400_BAD_REQUEST,
-                    )
-        return JSONResponse(
-            status_code=status.HTTP_302_FOUND,
-            content={"Unauthorized": "Session doesn't exist"},
-            headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
-        )
-    except ValueError as error:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail={"error": error}
-        )
+    if session:
+        hashed_name = parser.parse_link_hash(name)
+        room = database.get_room_by_name(hashed_name)
+        decoded_session = decoder.decode_session(session)
+        user = database.get_user_by_name(decoded_session["username"], room.id)
+        if decoder.verify_session(hashed_name, room.password, session) and user.status:
+            data = database.get_msg_key(room.id, decoded_session["username"])
+            key_session = decoder.session_add_key(session, data.key)
+            await database.delete_key(data.id)
+            return JSONResponse(
+                status_code=status.HTTP_200_OK,
+                content={"Status": "The encryption key was received"},
+                headers={
+                    "Set-Cookie": f"session={key_session}",
+                },
+            )
+    return JSONResponse(
+        status_code=status.HTTP_302_FOUND,
+        content={"Unauthorized": "Session doesn't exist"},
+        headers={"Location": f"/rooms/{name}/auth", "Connection": "close"},
+    )
 
 
 @router.websocket("/{name}")
@@ -302,4 +252,3 @@ async def websocket_endpoint(
                     await room.broadcast(202, f"{username} left chat", username)
             except RuntimeError:
                 manager.delete_connections(name)
-                
--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.exc import IntegrityError
 from db.create_tables import Rooms, Users, Messages, MsgKeys
+from fastapi import HTTPException, status
 from utils.crypt import Decoder
 from core.config import settings
 
@@ -25,26 +26,29 @@ class DtabaseHelper:
 
     def get_table(self, tableName: str) -> Table:
         tables = {
-            'Rooms': Rooms,
-            'Users': Users,
-            'Messages': Messages,
-            'MsgKeys': MsgKeys
+            "Rooms": Rooms,
+            "Users": Users,
+            "Messages": Messages,
+            "MsgKeys": MsgKeys,
         }
         return tables[tableName]
 
     def check_user(self, username: str, room_id: int) -> bool:
-        usersTable = self.get_table('Users')
+        usersTable = self.get_table("Users")
         with self.session as session:
-            user = session.query(usersTable).where(
-                usersTable.name == username, usersTable.room_id == room_id
-            ).all()
+            user = (
+                session.query(usersTable)
+                .where(usersTable.name == username, usersTable.room_id == room_id)
+                .all()
+            )
             return len(user) > 0
 
     def check_msg_key(self, username: int):
-        keyTable = self.get_table('MsgKeys')
+        keyTable = self.get_table("MsgKeys")
         with self.session as session:
-            key = session.query(keyTable).where(
-                keyTable.destinied_for == username).all()
+            key = (
+                session.query(keyTable).where(keyTable.destinied_for == username).all()
+            )
             return len(key) > 0
 
 
@@ -53,58 +57,66 @@ class DatabaseGet(DtabaseHelper):
         super().__init__(path, echo)
 
     def get_room_by_id(self, id: int) -> list:
-        roomTable = self.get_table('Rooms')
-        try:
-            with self.session as session:
-                room = session.query(roomTable).where(
-                    roomTable.id == id
-                ).all()
-                return room[0]
-        except IndexError:
-            raise ValueError("Room doesn't exist")
+        roomTable = self.get_table("Rooms")
+        with self.session as session:
+            room = session.query(roomTable).where(roomTable.id == id).first()
+            if not room:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="Room not found"
+                )
+            return room
 
     def get_room_by_name(self, name: str) -> list:
-        roomTable = self.get_table('Rooms')
-        try:
-            with self.session as session:
-                room = session.query(roomTable).where(
-                    roomTable.name == name
-                ).all()
-                return room[0]
-        except IndexError:
-            raise ValueError("Room doesn't exist")
+        roomTable = self.get_table("Rooms")
+        with self.session as session:
+            room = session.query(roomTable).where(roomTable.name == name).first()
+            if not room:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="Room not found"
+                )
+            return room
 
     def get_user_by_id(self, id: int) -> Users:
-        usersTable = self.get_table('Users')
-        try:
-            with self.session as session:
-                user = session.query(usersTable).where(
-                    usersTable.id == id
-                ).all()
-                return user[0]
-        except IndexError:
-            raise ValueError("User doesn't exist")
+        usersTable = self.get_table("Users")
+        with self.session as session:
+            user = session.query(usersTable).where(usersTable.id == id).first()
+            if not user:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+                )
+            return user
 
     def get_user_by_name(self, username: str, room_id: int) -> Users:
         room = self.get_room_by_id(room_id)
         names = list(map(lambda x: x.name, room.users))
-        ind = names.index(username)
-        return room.users[ind]
+        try:
+            ind = names.index(username)
+            return room.users[ind]
+        except ValueError:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="User not found"
+            )
 
     def get_room_users(self, room_id: int) -> list:
         room = self.get_room_by_id(room_id)
         return room.users
 
     def get_msg_key(self, room_id: int, destinied_for: str) -> MsgKeys:
-        keyTable = self.get_table('MsgKeys')
-        try:
-            with self.session as session:
-                key = session.query(keyTable).where(
-                    keyTable.room_id == room_id and keyTable.destinied_for == destinied_for
-                ).all()
-                return key[0]
-        except IndexError:
-            raise ValueError("Key doesn't exist")
+        keyTable = self.get_table("MsgKeys")
+        with self.session as session:
+            key = (
+                session.query(keyTable)
+                .where(
+                    keyTable.room_id == room_id
+                    and keyTable.destinied_for == destinied_for
+                )
+                .first()
+            )
+            if not key:
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND, detail="Key not found"
+                )
+            return key
 
     def get_all_messages(self, room_id: int):
         users = self.get_room_users(room_id)
@@ -121,27 +133,27 @@ class Database(DatabaseGet):
         super().__init__(path, echo)
 
     def create_room(self, data: Dict) -> bool:
-        roomTable = self.get_table('Rooms')
+        roomTable = self.get_table("Rooms")
         try:
             with self.session as session:
-                room = roomTable(name=data['name'], password=data['password'])
+                room = roomTable(name=data["name"], password=data["password"])
                 session.add(room)
                 session.commit()
-                self.create_user({
-                    'name': 'Admin',
-                    'admin': True,
-                    'room_id': room.id
-                }, room.id)
+                self.create_user(
+                    {"name": "Admin", "admin": True, "room_id": room.id}, room.id
+                )
                 return True
         except IntegrityError:
-            raise ValueError('roomName')
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Room name not unique"
+            )
 
     def create_user(self, data: Dict, room_id: int) -> bool:
-        userTable = self.get_table('Users')
+        userTable = self.get_table("Users")
         try:
             with self.session as session:
                 user = userTable(
-                    name=data['name'], admin=data['admin'], room_id=data['room_id']
+                    name=data["name"], admin=data["admin"], room_id=data["room_id"]
                 )
                 session.add(user)
                 room = self.get_room_by_id(room_id)
@@ -149,35 +161,33 @@ class Database(DatabaseGet):
                 session.commit()
             return True
         except IntegrityError:
-            raise ValueError('userName')
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST, detail="User name not unique"
+            )
 
     def create_message(self, message, user_id: int) -> bool:
-        try:
-            messagesTable = self.get_table('Messages')
-            user = self.get_user_by_id(user_id)
-            with self.session as session:
-                message = messagesTable(data=message, user_id=user_id)
-                session.add(message)
-                user.messages.append(message)
-                session.commit()
-                return True
-        except ValueError:
-            return False
+        messagesTable = self.get_table("Messages")
+        user = self.get_user_by_id(user_id)
+        with self.session as session:
+            message = messagesTable(data=message, user_id=user_id)
+            session.add(message)
+            user.messages.append(message)
+            session.commit()
 
     def create_msg_key(self, room_id: int, destinied_for: str, key: str) -> bool:
-        keyTable = self.get_table('MsgKeys')
+        keyTable = self.get_table("MsgKeys")
         if not self.check_msg_key(destinied_for):
             with self.session as session:
-                note = keyTable(
-                    room_id=room_id, destinied_for=destinied_for, key=key
-                )
+                note = keyTable(room_id=room_id, destinied_for=destinied_for, key=key)
                 session.add(note)
                 session.commit()
         else:
-            raise ValueError('Note already exists')
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT, detail="Key alredy created"
+            )
 
     def delete_key(self, keyId: int):
-        keyTable = self.get_table('MsgKeys')
+        keyTable = self.get_table("MsgKeys")
         with self.session as session:
             key = session.query(keyTable).where(keyTable.id == keyId).one()
             session.delete(key)
